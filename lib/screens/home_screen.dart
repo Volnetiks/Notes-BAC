ebf5eb357309b1a8783405f7b2f9846e1b3f2c4d
Volnetiks <volnetiks@gmail.com>
2021-12-07 19:02:04 +0100

    Moved HomeScreen body to a widget and switched to TabBarView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bac_note/models/grade.dart';
+import 'package:bac_note/screens/schedule_screen.dart';
 import 'package:bac_note/widgets/grade_tile.dart';
 import 'package:bac_note/widgets/grades_chart.dart';
 import 'package:flutter/material.dart';
@@ -14,17 +15,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
-  List<Grade> grades = [
-    Grade.fromResultOutOf20("Maths", "Chapter 5", DateTime(2010, 11, 9), 20),
-    Grade.fromResultOutOf20("History", "Chapter 1", DateTime(2010, 8, 9), 20),
-    Grade.fromResult("French", "Reading Test 3", DateTime(2010, 8, 9), 90),
-    Grade.fromResult("English", "New Zealand", DateTime(2010, 9, 9), 10),
-    Grade.fromResult("Polish", "Verbs", DateTime(2010, 12, 9), 70),
-    Grade.fromResult("IT", "Network", DateTime.now(), 40),
-  ];
-
-  String dropdownValue = 'Récentes';
-
   @override
   void initState() {
     super.initState();
@@ -55,128 +45,152 @@ class _HomePageState extends State<HomePage>
           final TabController tabController = DefaultTabController.of(context)!;
 
           return Scaffold(
-            appBar: AppBar(
-                centerTitle: true,
-                title: Column(
-                  children: [
-                    Text("Student name",
-                        style:
-                            TextStyle(color: Theme.of(context).primaryColor)),
-                    Text(
-                      "Notre Dame De Toutes Aides",
-                      style: TextStyle(
-                          fontSize: 12, color: Theme.of(context).disabledColor),
-                    )
-                  ],
-                ),
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(
-                    bottom: Radius.circular(30),
-                  ),
-                ),
-                backgroundColor: Theme.of(context).unselectedWidgetColor,
-                bottom: TabBar(
-                  indicatorSize: TabBarIndicatorSize.label,
-                  indicatorColor: Theme.of(context).primaryColor,
-                  tabs: tabs,
-                )),
-            body: SafeArea(
-              child: Column(
-                children: [
-                  GradesChart(grades: grades),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              appBar: AppBar(
+                  centerTitle: true,
+                  title: Column(
                     children: [
-                      const Padding(
-                        padding: EdgeInsets.only(left: 20),
-                        child: Text("Dernières notes",
-                            style: TextStyle(
-                                fontWeight: FontWeight.w800, fontSize: 20)),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(right: 20),
-                        child: DropdownButtonHideUnderline(
-                          child: DropdownButton<String>(
-                            value: dropdownValue,
-                            icon: Icon(Icons.arrow_downward),
-                            iconSize: 20,
-                            elevation: 16,
-                            style: TextStyle(
-                                color: Theme.of(context).primaryColor),
-                            underline: Container(
-                              height: 2,
-                              color: Theme.of(context).unselectedWidgetColor,
-                            ),
-                            onChanged: (String? newValue) {
-                              setState(() {
-                                dropdownValue = newValue!;
-                              });
-                            },
-                            items: ['Plus ancienne', "Récentes", "Notes"]
-                                .map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem(
-                                  value: value, child: Text(value));
-                            }).toList(),
-                          ),
-                        ),
-                      ),
+                      Text("Student name",
+                          style:
+                              TextStyle(color: Theme.of(context).primaryColor)),
+                      Text(
+                        "Notre Dame De Toutes Aides",
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: Theme.of(context).disabledColor),
+                      )
                     ],
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Flexible(
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: StaggeredGridView.countBuilder(
-                            crossAxisCount: 2,
-                            mainAxisSpacing: 20,
-                            itemCount: grades.length,
-                            shrinkWrap: true,
-                            physics: BouncingScrollPhysics(),
-                            itemBuilder: (context, index) {
-                              switch (dropdownValue) {
-                                case 'Plus ancienne':
-                                  grades
-                                      .sort((a, b) => a.date.compareTo(b.date));
-                                  break;
-
-                                case 'Récentes':
-                                  grades
-                                      .sort((a, b) => b.date.compareTo(a.date));
-                                  break;
-
-                                case 'Notes':
-                                  grades.sort((a, b) => b.resultOutOf20
-                                      .compareTo(a.resultOutOf20));
-                                  break;
-
-                                default:
-                                  grades
-                                      .sort((a, b) => a.date.compareTo(b.date));
-                                  break;
-                              }
-
-                              return GradeTile(
-                                grade: grades[index],
-                              );
-                            },
-                            staggeredTileBuilder: (int index) {
-                              return StaggeredTile.fit(2);
-                            }),
-                      ),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(30),
                     ),
                   ),
-                ],
+                  backgroundColor: Theme.of(context).unselectedWidgetColor,
+                  bottom: TabBar(
+                    indicatorSize: TabBarIndicatorSize.label,
+                    indicatorColor: Theme.of(context).primaryColor,
+                    tabs: tabs,
+                  )),
+              body: TabBarView(children: [
+                HomeScreenWidget(),
+                ScheduleScreen(),
+                HomeScreenWidget()
+              ]));
+        },
+      ),
+    );
+  }
+}
+
+class HomeScreenWidget extends StatefulWidget {
+  const HomeScreenWidget({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenWidgetState createState() => _HomeScreenWidgetState();
+}
+
+class _HomeScreenWidgetState extends State<HomeScreenWidget> {
+  List<Grade> grades = [
+    Grade.fromResultOutOf20("Maths", "Chapter 5", DateTime(2010, 11, 9), 20),
+    Grade.fromResultOutOf20("History", "Chapter 1", DateTime(2010, 8, 9), 20),
+    Grade.fromResult("French", "Reading Test 3", DateTime(2010, 8, 9), 90),
+    Grade.fromResult("English", "New Zealand", DateTime(2010, 9, 9), 10),
+    Grade.fromResult("Polish", "Verbs", DateTime(2010, 12, 9), 70),
+    Grade.fromResult("IT", "Network", DateTime.now(), 40),
+  ];
+
+  String dropdownValue = 'Récentes';
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          GradesChart(grades: grades),
+          const SizedBox(
+            height: 5,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text("Dernières notes",
+                    style:
+                        TextStyle(fontWeight: FontWeight.w800, fontSize: 20)),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 20),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    value: dropdownValue,
+                    icon: Icon(Icons.arrow_downward),
+                    iconSize: 20,
+                    elevation: 16,
+                    style: TextStyle(color: Theme.of(context).primaryColor),
+                    underline: Container(
+                      height: 2,
+                      color: Theme.of(context).unselectedWidgetColor,
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        dropdownValue = newValue!;
+                      });
+                    },
+                    items: ['Plus ancienne', "Récentes", "Notes"]
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem(value: value, child: Text(value));
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Flexible(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: StaggeredGridView.countBuilder(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 20,
+                    itemCount: grades.length,
+                    shrinkWrap: true,
+                    physics: BouncingScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      switch (dropdownValue) {
+                        case 'Plus ancienne':
+                          grades.sort((a, b) => a.date.compareTo(b.date));
+                          break;
+
+                        case 'Récentes':
+                          grades.sort((a, b) => b.date.compareTo(a.date));
+                          break;
+
+                        case 'Notes':
+                          grades.sort((a, b) =>
+                              b.resultOutOf20.compareTo(a.resultOutOf20));
+                          break;
+
+                        default:
+                          grades.sort((a, b) => a.date.compareTo(b.date));
+                          break;
+                      }
+
+                      return GradeTile(
+                        grade: grades[index],
+                      );
+                    },
+                    staggeredTileBuilder: (int index) {
+                      return StaggeredTile.fit(2);
+                    }),
               ),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
