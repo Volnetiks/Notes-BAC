b29eb803d18260b77e3225e9081a00e0f4caa096
Volnetiks <volnetiks@gmail.com>
2022-01-01 10:30:32 +0100

    Fixed test folder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +14,11 @@ import 'package:bac_note/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    final AdaptiveThemeMode? savedThemeMode =
+        await AdaptiveTheme.getThemeMode();
+    await tester.pumpWidget(MyApp(
+      savedThemeMode: savedThemeMode,
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
